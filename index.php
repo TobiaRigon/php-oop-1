658b6dfcb244cb23a7402fe26e49dd7a426b9b5f
TobiaRigon <rigontobia@gmail.com>
2024-02-02 14:47:24 +0100

    Feat:Bonus 1 Multiple geners

--- index.php
+++ index.php
@@ -1,60 +1,64 @@
 <?php
 
-    class Movie {
+class Movie {
 
-        public $title;
-        public $directed;
-        public $language;
+    public $title;
+    public $director;
+    public $language;
+    public $genres;
 
-        public function __construct($title, $directed, $language) {
+    public function __construct($title, $director, $language, $genres) {
 
-            $this -> title = $title;
-            $this -> directed = $directed;
-            $this -> language = $language;
-        }
-
-        public function getTitle() {
-
-            return $this -> title;
-        }
-        public function setTitle($title) {
-
-            $this -> title = $title;
-        }
+        $this->title = $title;
+        $this->director = $director;
+        $this->language = $language;
+        $this->genres = $genres;
     }
 
-    // class User {
+    // public function getTitle() {
 
-    //     public $nome;
-    //     public $eta;
-    //     public  $address;
-
-    //     function __construct($nome, $eta,  $address) {
-
-    //         $this -> nome = $nome;
-    //         $this -> eta = $eta;
-    //         $this -> address = $address;
-    //     }
+    //     return $this->title;
     // }
 
-    $movie1 = new Movie("Snatch", "Guy Ritchie", "English");
-    $movie2 = new Movie("RRR", "S. S. Rajamouli", "Telugu");
+    // public function setTitle($title) {
 
-   
+    //     $this->title = $title;
+    // }
 
-    var_dump($movie1);
-    echo "<br><br>";
-    var_dump($movie1 -> title);
-    echo "<br>";
-    var_dump($movie1 -> directed);
-    echo "<br>";
-    var_dump($movie1 -> language);
-    echo "<br>-----------------<br>";
-    var_dump($movie2);
-    echo "<br><br>";
-    var_dump($movie2 -> title);
-    echo "<br>";
-    var_dump($movie2 -> directed);
-    echo "<br>";
-    var_dump($movie2 -> language);
+    public function getGenres() {
 
+        return $this->genres;
+    }
+
+    public function setGenres($genres) {
+
+        $this->genres = $genres;
+    }
+}
+
+$genres1 = ["Crime", "Comedy"];
+$genres2 = ["Action", "Drama"];
+
+$movie1 = new Movie("Snatch", "Guy Ritchie", "English", $genres1);
+$movie2 = new Movie("RRR", "S. S. Rajamouli", "Telugu", $genres2);
+
+var_dump($movie1);
+echo "<br><br>";
+var_dump($movie1->title);
+echo "<br>";
+var_dump($movie1->director);
+echo "<br>";
+var_dump($movie1->language);
+echo "<br>";
+var_dump($movie1->getGenres());
+echo "<br>-----------------<br>";
+var_dump($movie2);
+echo "<br><br>";
+var_dump($movie2->title);
+echo "<br>";
+var_dump($movie2->director);
+echo "<br>";
+var_dump($movie2->language);
+echo "<br>";
+var_dump($movie2->getGenres());
+?>
