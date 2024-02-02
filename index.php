5cb5378fc203c3881504c60d8e68ab2804ae1ea0
TobiaRigon <rigontobia@gmail.com>
2024-02-02 17:18:22 +0100

    Fix: Console error favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="https://cdn.icon-icons.com/icons2/12/PNG/256/movies_movie_oscar_themedia_1735.png">
     <title>List of Movies</title>
     <link rel="stylesheet" href="./style.css">
 </head>
